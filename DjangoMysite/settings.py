76e0f9866461150ee5129ff6c3b7141d954ebfcf
kliu <batianhu2036@126.com>
2022-02-23 16:26:14 +0800

    update

--- DjangoMysite/settings.py
+++ DjangoMysite/settings.py
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'DjangoMysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gx_day15', #数据库名字
+        'USER': 'root', #账户名
+        'PASSWORD':'admin@123', #密码
+        'HOST':'127.0.0.1',#mysql是哪台设备
+        'PORT':'3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- siteapp/models.py
+++ siteapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class UserInfo(models.Model):
+    name = models.CharField(max_length=32)
+    password = models.CharField(max_length=32)
+    age = models.IntegerField(default = 10)
+    data = models.IntegerField(null=True,blank=True)
+
+class Department(models.Model):
+    title = models.CharField(max_length=16)
+
+
+class Role(models.Model):
+    caption = models.CharField(max_length=16)
+
+
+
+"""
+在models创建类
+ORM 会根据这个类区创建相应的表项
+create table siteapp_userinfo(
+    id bigint auto_increment primary key,
+    name varchar(32)
+    password varchar(64),
+    age int
+)
+在命令行执行两个命令
+1.python manage.py makemigrations
+2.python manage.py migrate
+app必须先注册才可以
+"""
